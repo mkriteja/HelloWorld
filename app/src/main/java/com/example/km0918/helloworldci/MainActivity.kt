bd85f5615c5b48aea0da888bff3038b47ef04ffe
Krishna Medavarapu <km0918@M524.dexcom.dexcominc.com>
2018-10-28 18:15:24 -0700

    Test commit

--- app/src/main/java/com/example/km0918/helloworldci/MainActivity.kt
+++ app/src/main/java/com/example/km0918/helloworldci/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Hello
         setContentView(R.layout.activity_main)
     }
 }
